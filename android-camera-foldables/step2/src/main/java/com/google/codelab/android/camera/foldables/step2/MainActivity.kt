1c547415447018f727b7ef59dcaf3e60f8b39c7a
romanofranz <romanofranz@google.com>
2023-09-20 17:02:17 +0200

    updated to wm 1.2-beta01, now there is no need to store the session in the viewmodel, also updated various libraries and icons

--- android-camera-foldables/step2/src/main/java/com/google/codelab/android/camera/foldables/step2/MainActivity.kt
+++ android-camera-foldables/step2/src/main/java/com/google/codelab/android/camera/foldables/step2/MainActivity.kt
@@ -22,17 +22,24 @@ import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.window.area.WindowAreaCapability
 import androidx.window.area.WindowAreaController
+import androidx.window.area.WindowAreaInfo
 import androidx.window.area.WindowAreaSession
 import androidx.window.area.WindowAreaSessionCallback
 import androidx.window.core.ExperimentalWindowApi
-import androidx.window.java.area.WindowAreaControllerJavaAdapter
 import com.google.codelab.android.camera.foldables.databinding.ActivityMainBinding
 import com.google.codelab.android.camera.foldables.step2.camera2.CameraLifecycleObserver
 import com.google.codelab.android.camera.foldables.step2.camera2.CameraViewFinderActor
 import com.google.codelab.android.camera.foldables.step2.utils.Log
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import java.util.*
 import java.util.concurrent.Executor
@@ -57,8 +64,13 @@ class MainActivity : AppCompatActivity(), WindowAreaSessionCallback {
     lateinit var actor: CameraViewFinderActor
 
     /**Codelab: Add variables for rear display mode **/
-    private lateinit var windowAreaController: WindowAreaControllerJavaAdapter
+    private lateinit var windowAreaController: WindowAreaController
     private lateinit var displayExecutor: Executor
+    private var rearDisplaySession: WindowAreaSession? = null
+    private var rearDisplayWindowAreaInfo: WindowAreaInfo? = null
+    private var rearDisplayStatus: WindowAreaCapability.Status =
+        WindowAreaCapability.Status.WINDOW_AREA_STATUS_UNSUPPORTED
+    private val rearDisplayOperation = WindowAreaCapability.Operation.OPERATION_TRANSFER_ACTIVITY_TO_AREA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,11 +87,25 @@ class MainActivity : AppCompatActivity(), WindowAreaSessionCallback {
         binding.toggle.setOnClickListener { viewModel.toggleCamera() }
         binding.capture.setOnClickListener { viewModel.capture() }
         binding.aspectRatio.setOnClickListener { viewModel.toggleAspectRatio() }
-        binding.rearDisplay.setOnClickListener { startRearDisplayMode() }
+        binding.rearDisplay.setOnClickListener { toggleRearDisplayMode() }
 
-        /**Codelab: initialize WindowInfoTracker**/
+        /**Codelab: initialize WindowAreaController*/
         displayExecutor = ContextCompat.getMainExecutor(this)
-        windowAreaController = WindowAreaControllerJavaAdapter(WindowAreaController.getOrCreate())
+        windowAreaController = WindowAreaController.getOrCreate()
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                windowAreaController.windowAreaInfos
+                    .map { info -> info.firstOrNull { it.type == WindowAreaInfo.Type.TYPE_REAR_FACING } }
+                    .onEach { info -> rearDisplayWindowAreaInfo = info }
+                    .map { it?.getCapability(rearDisplayOperation)?.status ?: WindowAreaCapability.Status.WINDOW_AREA_STATUS_UNSUPPORTED }
+                    .distinctUntilChanged()
+                    .collect {
+                        rearDisplayStatus = it
+                        updateUI()
+                    }
+            }
+        }
     }
 
     override fun onResume() {
@@ -92,27 +118,66 @@ class MainActivity : AppCompatActivity(), WindowAreaSessionCallback {
     }
 
     /** Codelab: add the code to manage rear display sessions **/
-
-    private fun startRearDisplayMode() {
-        if (viewModel.rearDisplaySession != null) {
-            viewModel.rearDisplaySession?.close()
+    private fun toggleRearDisplayMode() {
+        if(rearDisplayStatus == WindowAreaCapability.Status.WINDOW_AREA_STATUS_ACTIVE) {
+            if(rearDisplaySession == null) {
+                rearDisplaySession = rearDisplayWindowAreaInfo?.getActiveSession(
+                    rearDisplayOperation
+                )
+            }
+            rearDisplaySession?.close()
         } else {
-            windowAreaController.startRearDisplayModeSession(
-                this,
-                displayExecutor,
-                this
-            )
+            rearDisplayWindowAreaInfo?.token?.let { token ->
+                windowAreaController.transferActivityToWindowArea(
+                    token = token,
+                    activity = this,
+                    executor = displayExecutor,
+                    windowAreaSessionCallback = this
+                )
+            }
         }
     }
 
-    override fun onSessionEnded() {
-        viewModel.updateSession(null)
-        Log.d("rear session onSessionEnded")
+    private fun updateUI() {
+        if(rearDisplaySession != null) {
+            binding.rearDisplay.isEnabled = true
+            // A session is already active, clicking on the button will disable it
+        } else {
+            when(rearDisplayStatus) {
+                WindowAreaCapability.Status.WINDOW_AREA_STATUS_UNSUPPORTED -> {
+                    binding.rearDisplay.isEnabled = false
+                    // RearDisplay Mode is not supported on this device"
+                }
+                WindowAreaCapability.Status.WINDOW_AREA_STATUS_UNAVAILABLE -> {
+                    binding.rearDisplay.isEnabled = false
+                    // RearDisplay Mode is not currently available
+                }
+                WindowAreaCapability.Status.WINDOW_AREA_STATUS_AVAILABLE -> {
+                    binding.rearDisplay.isEnabled = true
+                    // You can enable RearDisplay Mode
+                }
+                WindowAreaCapability.Status.WINDOW_AREA_STATUS_ACTIVE -> {
+                    binding.rearDisplay.isEnabled = true
+                    // You can disable RearDisplay Mode
+                }
+                else -> {
+                    binding.rearDisplay.isEnabled = false
+                    // RearDisplay status is unknown
+                }
+            }
+        }
+    }
+
+    override fun onSessionEnded(t: Throwable?) {
+        if(t != null) {
+            Log.d("Something was broken: ${t.message}")
+        }else{
+            Log.d("rear session ended")
+        }
     }
 
     override fun onSessionStarted(session: WindowAreaSession) {
-        viewModel.updateSession(session)
-        Log.d("rear session onSessionStarted [session=${viewModel.rearDisplaySession}]")
+        Log.d("rear session started [session=$session]")
     }
     //endregion
 }
